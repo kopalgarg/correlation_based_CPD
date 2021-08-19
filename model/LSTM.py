94f467dc305845a6ff7b1664f608f22e0512e08d
kopalgarg <kopalgarg24@gmail.com>
2021-08-19 03:37:45 -0600

    Create LSTM.py

---
+++ model/LSTM.py
@@ -0,0 +1,24 @@
+
+from keras import regularizers
+from keras.models import Sequential
+from keras.layers import LSTM
+from keras.layers import Dense
+from keras.layers import Activation
+from keras.layers import Dropout
+from keras.layers import Flatten
+
+
+def LSTM(n_steps, n_features):
+    model = tf.keras.Sequential()
+    model.add(tf.keras.layers.LSTM(128, input_shape=(n_steps, n_features), return_sequences=True))
+    model.add(tf.keras.layers.LeakyReLU(alpha=0.5)) 
+    model.add(tf.keras.layers.LSTM(64, return_sequences=True))
+    model.add(tf.keras.layers.LeakyReLU(alpha=0.5)) 
+    model.add(tf.keras.layers.Dropout(0.3)) 
+    model.add(tf.keras.layers.LSTM(28, return_sequences=False))
+    model.add(tf.keras.layers.Dropout(0.3)) 
+    model.add(tf.keras.layers.Dense(1))
+    
+    model.compile(optimizer='adam', loss='mse')
+    print(model.summary())
+    return model
